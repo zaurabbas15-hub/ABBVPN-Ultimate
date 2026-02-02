f860851f6e522510cd4b69b40d22cb2e8bc508f2
zaurabbas15-hub <zaurabbas15@gmail.com>
2026-02-02 22:49:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,14 +11,17 @@ let isQuiting = false;
 
 const hwid = machineIdSync();
 
-// ТВОИ ДАННЫЕ
-const KEYS_URL = "https://gist.githubusercontent.com/zaurabbas15-hub/4c04c7980c3537479d754e52dcaab245/raw/gistfile1.txt";
-const vlessLink = "vless://9c9c27db-c405-4a18-938b-78ef5c6e6aa6@109.120.177.8:443?encryption=none&fp=chrome&pbk=7HKzYgCuVtDgZqe8C1a1Elrd4u3-GIhz92DFT9RzgCo&security=reality&sid=c5dd15&sni=www.microsoft.com&spx=%2FuD2s5qdYCoM1sRp&type=tcp#ABBVPN-1gf5b6ek";
+// --- СЕКРЕТНЫЕ ДАННЫЕ УДАЛЕНЫ ДЛЯ БЕЗОПАСНОСТИ ---
+const KEYS_URL = "ССЫЛКА_НА_ВАШ_GIST_С_КЛЮЧАМИ";
+const vlessLink = "ВАША_VLESS_ССЫЛКА";
 
+// ПУТЬ К ФАЙЛУ ПАМЯТИ
 const licenseFilePath = path.join(app.getPath('userData'), 'license.txt');
 
+// 1. ОТПРАВКА HWID
 ipcMain.on('get-hwid', (event) => { event.reply('hwid-data', hwid); });
 
+// 2. ПРОВЕРКА ЛИЦЕНЗИИ (КЛЮЧ:ID)
 ipcMain.on('check-license', async (event, userKey) => {
     try {
         const response = await axios.get(KEYS_URL);
@@ -32,6 +35,7 @@ ipcMain.on('check-license', async (event, userKey) => {
     } catch (e) { event.reply('license-result', false); }
 });
 
+// 3. ЗАГРУЗКА ПАМЯТИ
 ipcMain.on('load-saved-key', (event) => {
     if (fs.existsSync(licenseFilePath)) {
         event.reply('key-loaded', fs.readFileSync(licenseFilePath, 'utf8'));
@@ -67,6 +71,7 @@ function killAll() {
     exec('reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f');
 }
 
+// ФУНКЦИЯ СОЗДАНИЯ КОНФИГА (ДАННЫЕ ЗАМЕНЕНЫ НА ПУСТЫЕ)
 function createXrayConfig() {
     const config = {
         "inbounds": [{ "port": 10808, "protocol": "socks", "settings": { "auth": "noauth", "udp": true } }],
@@ -74,17 +79,29 @@ function createXrayConfig() {
             "protocol": "vless",
             "settings": {
                 "vnext": [{
-                    "address": "109.120.177.8", "port": 443,
-                    "users": [{ "id": "9c9c27db-c405-4a18-938b-78ef5c6e6aa6", "encryption": "none" }]
+                    "address": "ВАШ_IP_АДРЕС", 
+                    "port": 443,
+                    "users": [{ "id": "ВАШ_UUID", "encryption": "none" }]
                 }]
             },
-            "streamSettings": { "network": "tcp", "security": "reality", "realitySettings": { "serverName": "www.microsoft.com", "fingerprint": "chrome", "show": false, "publicKey": "7HKzYgCuVtDgZqe8C1a1Elrd4u3-GIhz92DFT9RzgCo", "shortId": "c5dd15", "spiderX": "/uD2s5qdYCoM1sRp" } }
+            "streamSettings": { 
+                "network": "tcp", 
+                "security": "reality", 
+                "realitySettings": { 
+                    "serverName": "www.microsoft.com", 
+                    "fingerprint": "chrome", 
+                    "show": false, 
+                    "publicKey": "ВАШ_ПУБЛИЧНЫЙ_КЛЮЧ", 
+                    "shortId": "ВАШ_SHORT_ID", 
+                    "spiderX": "ВАШ_SPIDER_X" 
+                } 
+            }
         }]
     };
     fs.writeFileSync(path.join(app.isPackaged ? path.dirname(app.getPath('exe')) : app.getAppPath(), 'config.json'), JSON.stringify(config, null, 4));
 }
 
-// ПРОВАЙДЕРЫ (Настройки)
+// ПРОВАЙДЕРЫ
 function getGdpiArgs(provider) {
     const base = "--blacklist russia-blacklist.txt";
     switch (provider) {
@@ -103,14 +120,12 @@ ipcMain.on('start-vpn', (event, mode) => {
         const workDir = path.dirname(process.execPath);
         if (mode === 'vless') {
             createXrayConfig();
-            // ФОНОВЫЙ ЗАПУСК XRAY
             exec(`start /B xray.exe run -c config.json`, { cwd: workDir });
             setTimeout(() => {
                 exec('reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f');
                 exec('reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v ProxyServer /t REG_SZ /d "socks5://127.0.0.1:10808" /f');
             }, 1500);
         } else {
-            // ФОНОВЫЙ ЗАПУСК GOODBYEDPI (Никаких окон!)
             exec(`start /B goodbyedpi.exe ${getGdpiArgs(mode)}`, { cwd: workDir });
         }
     }, 200);
